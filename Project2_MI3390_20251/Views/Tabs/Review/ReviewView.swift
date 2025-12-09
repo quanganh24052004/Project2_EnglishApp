e4edb9fe98a2842a85b4eaac6853b9bea0641b9c
Anh Nguyen Quang <anh.nguyenquang24052004@gmail.com>
2025-12-09 07:43:28 +0700

    feat: add Review View

---
+++ Project2_MI3390_20251/Views/Tabs/Review/ReviewView.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewView.swift
+//  Project2_MI3390_20251
+//
+//  Created by Nguyễn Quang Anh on 9/12/25.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Xin chào")
+        }
+        .navigationTitle(Text("Review"))
+    }
+}
+
+#Preview {
+    ReviewView()
+}

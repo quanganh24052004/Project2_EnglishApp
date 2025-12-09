2dc63a42313eba05e94193a075c901c271b55e0b
Anh Nguyen Quang <anh.nguyenquang24052004@gmail.com>
2025-12-09 12:57:02 +0700

    chore: delete comment

--- Project2_MI3390_20251/App/RootView.swift
+++ Project2_MI3390_20251/App/RootView.swift
@@ -16,25 +16,18 @@ struct RootView: View {
     var body: some View {
         ZStack {
             if showSplash {
-                // Luôn hiển thị cái này đầu tiên
                 SplashScreenView()
-                    .transition(.opacity) // Hiệu ứng mờ dần khi tắt
-                    .zIndex(1) // Đảm bảo luôn nằm trên cùng
+                    .transition(.opacity)
+                    .zIndex(1)
             } else {
-                // Sau khi Splash tắt, mới check logic nghiệp vụ
                 if isOnboardingDone {
-                    // Màn hình chính sau khi đã hoàn thành khảo sát
-                    // (Bạn có thể thay bằng MainTabView hoặc HomeView của bạn)
                     MainTabView()
                 } else {
-                    // Màn hình khảo sát (Code bạn đã gửi)
                     SurveyView(isOnboardingDone: $isOnboardingDone)
                 }
             }
         }
         .onAppear {
-            // Giả lập thời gian loading (ví dụ 2.5 giây)
-            // Trong thực tế, bạn có thể gọi API tải config ở đây
             DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
                 withAnimation {
                     showSplash = false
